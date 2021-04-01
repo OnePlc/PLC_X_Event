2fb1ff0d9d78a87da2062e7e1bfa3b8af61ad1bf
Praesidiarius <n.kammermann@swiss-crypto-mining.com>
2021-04-01 11:53:14 +0200

    templates moved to core

--- data/install.sh
+++ data/install.sh
@@ -2,9 +2,19 @@
 
 DIR="/var/www/plc"
 
-echo "${DIR}/vendor/oneplace/oneplace-event/public/ ${DIR}/public/vendor/oneplace-event"
-ln -s "${DIR}/public/vendor/@fullcalendar/" "${DIR}/vendor/oneplace/oneplace-event/public/lib/@fullcalendar"
+if [ ! -d "${DIR}/vendor/oneplace/oneplace-event/public/lib" ]; then
+  mkdir "${DIR}/vendor/oneplace/oneplace-event/public/lib"
+  mkdir "${DIR}/vendor/oneplace/oneplace-event/public/lib/@fullcalendar"
+  cd "${DIR}/vendor/oneplace/oneplace-event/public/lib/@fullcalendar"
+  wget "https://github.com/fullcalendar/fullcalendar/releases/download/v5.6.0/fullcalendar-5.6.0.zip"
+  unzip fullcalendar-5.6.0.zip
+  mv lib/* .
+  rm -rf lib/
+fi
+
 ln -s "${DIR}/vendor/oneplace/oneplace-event/public/" "${DIR}/public/vendor/oneplace-event"
+echo "Please add this softlink manually if it did not work"
+echo "ln -s ${DIR}/vendor/oneplace/oneplace-event/public/ ${DIR}/public/vendor/oneplace-event"
 
 cd "${DIR}"
 yarn add bootstrap-colorpicker
